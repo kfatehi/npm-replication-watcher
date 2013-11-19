fd8d84afb82f72746cc77633a2417a27121617b5
root <root@localhost.localdomain>
2013-11-19 08:19:48 +0000

    Add couchdb autorestart

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   couchdb: {
+		restart: "systemctl start couchdb",
     uri: "http://127.0.0.1:5984",
     replicate: {
       "source":"http://isaacs.iriscouch.com/registry/",
@@ -8,7 +9,7 @@ module.exports = {
       "create_target":true
     }
   },
-  checkFrequencyInSeconds: 5,
-  followupFrequencyInHours: 24,
-  waitTimeAfterRetriggerInSeconds: 30
+  checkFrequencyInSeconds: 480,
+  followupFrequencyInHours: 6,
+  waitTimeAfterRetriggerInSeconds: 120
 }
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var request = require('request-json'),
+    exec = require('child_process').exec,
     config = require(__dirname+'/config.js'),
     replicate = config.couchdb.replicate,
     local = request.newClient(config.couchdb.uri),
     remote = request.newClient(replicate["source"]),
     frequency = config.checkFrequencyInSeconds,
     followup = config.followupFrequencyInHours,
-    waitTime = config.waitTimeAfterRetriggerInSeconds;
+    waitTime = config.waitTimeAfterRetriggerInSeconds,
+		restartCount = 0;
+
+function crashRecovery() {
+  console.log("CouchDB has crashed.");
+  if (typeof(config.couchdb.restart) === "string")
+    exec(config.couchdb.restart, function(err,stdout,stderr){
+  		if (err !== null)
+  			console.log("Could not restart couchdb with command "+config.couchdb.restart);
+			else {
+				console.log("Restarted CouchDB a total of "+(++restartCount)+" times");
+			}
+    });
+  else
+    console.log("Set a command on the key config.couchdb.restart if you'd like it autorestarted");
+}
 
 function getLocalDiskSize(cb){
-  local.get('/registry', function(e,r,b) { cb(b.disk_size) })
+  if (typeof(local) !== "undefined")
+		local.get('/registry', function(e,r,b) { 
+			if (e)
+				crashRecovery();
+		  else
+				cb(b.disk_size);
+		});
+	else
+		crashRecovery()
 }
 
 function getRemoteDiskSize(cb){
